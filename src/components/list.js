ce50a4a20988c0ad076337a391f639529655bdd3
rojrun <runroj@gmail.com>
2018-12-19 16:32:52 -0800

    Completed displaying list items with dummy data

---
+++ src/components/list.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import dummyList from '../data/to-do-list';
+
+class List extends Component {
+    state = {
+        list: []
+    }
+
+    componentDidMount() {
+        this.getListData();
+    }
+
+    getListData(){
+        //Call server to get list data
+        this.setState({
+            list: dummyList
+        });
+    }
+
+    render() {
+        const listElements = this.state.list.map((item) => {
+            return <li key={item._id} className="collection-item">{item.title}</li>
+        });
+        return (
+            <ul className="collection">
+                {listElements}
+            </ul>
+        );
+    }
+}
+
+export default List;
