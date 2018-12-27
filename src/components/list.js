30241a8c3d0a53da6af030c79a3cd40ac10159df
rojrun <runroj@gmail.com>
2018-12-27 13:29:39 -0800

    single item view in progress

--- src/components/list.js
+++ src/components/list.js
@@ -5,8 +5,7 @@ import NavButton from "./nav_button";
 
 const List = (props) => {
         const listElements = props.toDos.map((item) => {
-            return <ListItem /*put={()=> props.put(item.complete)}*/
-                             delete={() => props.delete(item._id)} key={item._id} title={item.title}/>
+            return <ListItem itemId={item._id} delete={() => props.delete(item._id)} key={item._id} title={item.title}/>
         });
         return (
             <div>
--- src/components/list_item.js
+++ src/components/list_item.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default (props) => {
     return (
         <li className="collection-item row">
-            <form action="#">
-                <label className="col s8 ">
-                    <input onClick={props.put} type="checkbox"/>
-                    {/*<span>Complete</span>*/}
-                </label>
-            </form>
+            {/*<form action="#">*/}
+                {/*<label className="col s8 ">*/}
+                    {/*<input onClick={props.put} type="checkbox"/>*/}
+                    {/*/!*<span>Complete</span>*!/*/}
+                {/*</label>*/}
+            {/*</form>*/}
             <div className="col s10">
-                {props.title}
+                <Link to={`/item/${props.itemId}`}>{props.title}</Link>
             </div>
             <div className="col s2 center">
                 <button onClick={props.delete} className="btn btn-floating red darken-2">
